87be7edee9b8b637350f96dca6823a7e4162f8ff
radek <rdwwojcik@gmail.com>
2023-07-04 13:39:41 +0200

    Fix queue name

--- src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
+++ src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
@@ -14,7 +14,7 @@ import org.springframework.jms.support.converter.MessageType;
 public class JmsConfig {
 
     public static final String BREWING_REQUEST_QUEUE = "brewing-request";
-    public static final String NEW_INVENTORY_QUEUE = "NEW-INVENTORY";
+    public static final String NEW_INVENTORY_QUEUE = "new-inventory";
 
     @Bean
     public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper){
