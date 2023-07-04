a5d348ec31bc5a619df5fde324aefbbd831a6088
radek <rdwwojcik@gmail.com>
2023-07-04 08:34:21 +0200

    Configure JMS

---
+++ src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
@@ -0,0 +1,22 @@
+package guru.springframework.msscbeerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * Created by radek on 2023-07-04
+ */
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
