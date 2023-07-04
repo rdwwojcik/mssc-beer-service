66d75dd6e59e14a0f2649d02704d929a1621168d
radek <rdwwojcik@gmail.com>
2023-07-04 13:15:08 +0200

    Fix Jackson Configuration

--- src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
+++ src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
@@ -1,9 +1,7 @@
 package guru.springframework.msscbeerservice.events;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
-import lombok.Builder;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 
@@ -11,11 +9,12 @@ import java.io.Serializable;
  * Created by radek on 2023-07-04
  */
 @Data
-@RequiredArgsConstructor
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BeerEvent implements Serializable {
 
     static final long serialVersionUID = -5215973007899621882L;
 
-    private final BeerDto beerDto;
+    private BeerDto beerDto;
 }
--- src/main/java/guru/springframework/msscbeerservice/events/NewInventoryEvent.java
+++ src/main/java/guru/springframework/msscbeerservice/events/NewInventoryEvent.java
@@ -1,10 +1,12 @@
 package guru.springframework.msscbeerservice.events;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by radek on 2023-07-04
  */
+@NoArgsConstructor
 public class NewInventoryEvent extends BeerEvent{
 
     public NewInventoryEvent(BeerDto beerDto) {
